2b49ad7b5622471ad352d65be35afe992fc1a902
gozzafadillah <74459029+gozzafadillah@users.noreply.github.com>
2022-02-26 00:11:43 +0700

    Revisi tampilan esterik

--- 4_Basic_Programming/praktikum/problem_7.go
+++ 4_Basic_Programming/praktikum/problem_7.go
@@ -5,10 +5,13 @@ import "fmt"
 func playWithAsterik(n int) {
 	// variable
 	var asterik string
-	var star string = "*"
+	var star string = "* "
 
 	// code
 	for i := 1; i <= n; i++ {
+		for j := n - i; j >= 1; j-- {
+			fmt.Print(" ")
+		}
 		asterik += star
 		fmt.Println(asterik)
 	}
