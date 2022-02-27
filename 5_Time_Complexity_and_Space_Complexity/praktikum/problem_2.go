87235ee12bc2bdc8f3a0c4bebf1ccbe3c2ee06d7
gozzafadillah <74459029+gozzafadillah@users.noreply.github.com>
2022-02-27 16:49:52 +0700

    Push problem 2

---
+++ 5_Time_Complexity_and_Space_Complexity/praktikum/problem_2.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+)
+
+func pow(base, kuadrat int) int {
+	// variable
+	var y int = 1
+	var hasil int = 1
+
+	if kuadrat < 0 {
+		base = 1 / base
+		kuadrat = -kuadrat
+	}
+
+	if kuadrat == 0 {
+		y = 1
+	}
+
+	for i := 1; i <= kuadrat; i++ {
+		if kuadrat%2 == 0 {
+			base = base * base
+			kuadrat = kuadrat / 2
+		} else {
+			y = base * y
+			base = base * base
+			kuadrat = (kuadrat - 1) / 2
+		}
+	}
+	hasil = base * y
+
+	return hasil
+
+}
+
+func main() {
+	var base int
+	var kuadrat int
+
+	fmt.Print("Masukan angka : ")
+	fmt.Scanf("%d \n", &base)
+	fmt.Print("Masukan pangkat : ")
+	fmt.Scanf("%d \n", &kuadrat)
+
+	fmt.Println("===================>>>>>>>>>>>>>>>> \n")
+
+	fmt.Println("Hasil pangkat dari", base, "^", kuadrat, "=", pow(base, kuadrat))
+}
