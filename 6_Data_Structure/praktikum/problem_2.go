42280ddd4ae0aca3796e1969f176b6c1fdf1e8d0
gozzafadillah <74459029+gozzafadillah@users.noreply.github.com>
2022-02-28 15:29:06 +0700

    push problem 2 angka  muncul sekali

---
+++ 6_Data_Structure/praktikum/problem_2.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func angkaMunculSekali(angka string) []int {
+	// Variable
+	counter := map[string]int{}
+	// string di split
+	stringSplit := strings.Split(angka, "")
+	// menghitung jumlah data yang sama
+	for _, item := range stringSplit {
+		counter[item]++
+	}
+	// Membuat variable hasil dengan sesuai ukuran
+	hasil := make([]int, 0, len(counter))
+	// diubah menjadi int dan masukan ke array
+	for key, value := range counter {
+		// jika value 1 == true / tidak kembar
+		if value == 1 {
+			item, err := strconv.Atoi(key)
+			if err != nil {
+				panic(err)
+			}
+			hasil = append(hasil, item)
+		}
+	}
+
+	return hasil
+}
+
+func main() {
+	fmt.Println(angkaMunculSekali("1123982"))
+}
