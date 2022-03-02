908b45bd4dda72cadc909a03d1b38c8ceb28e5e7
gozzafadillah <74459029+gozzafadillah@users.noreply.github.com>
2022-03-02 20:03:02 +0700

    Push problem 2 first

---
+++ 7_String-Advance_Function-Pointer-Method-Struct_and_Interface/praktikum/problem_2.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func Caesar(offset int, input string) string {
+	var output string
+	var temp rune
+
+	for _, value := range input {
+		// bila lowercase
+		temp = ((value + rune(offset) - 97) % 26) + 97
+		// Output
+		output += strconv.QuoteRune(temp)
+	}
+
+	return output
+}
+
+func main() {
+	fmt.Println(Caesar(3, "abc"))
+	fmt.Println(Caesar(1, "abcdefghijklmnopqrstuvwxyz"))
+	fmt.Println(Caesar(10, "alterraacademy"))
+	fmt.Println(Caesar(1000, "abcdefghijklmnopqrstuvwxyz"))
+}
