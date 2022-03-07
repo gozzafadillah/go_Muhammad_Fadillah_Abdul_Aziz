11676e73961ae0c4c33abc6f5fb84d84773e2ef6
gozzafadillah <74459029+gozzafadillah@users.noreply.github.com>
2022-03-07 23:39:54 +0700

    fix logic max sum

--- 8_Recursive-Number_Theory-Sorting-Searching/Praktikum/problem_4.go
+++ 8_Recursive-Number_Theory-Sorting-Searching/Praktikum/problem_4.go
@@ -1,32 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func maxSequent(arr []int) int {
-	var isDone = false
-	for !isDone {
-		isDone = true
-		var i = 0
-		for i < len(arr)-1 {
-			if arr[i] > arr[i+1] {
-				arr[i], arr[i+1] = arr[i+1], arr[i]
-				isDone = false
-			}
-			i++
+	// variable
+	max_so_far := math.MinInt
+	max_ending_here := 0
+
+	for i := 0; i < len(arr)-1; i++ {
+		max_ending_here = max_ending_here + arr[i]
+		if max_so_far < max_ending_here {
+			max_so_far = max_ending_here
+		}
+		if max_ending_here < 0 {
+			max_ending_here = 0
 		}
 	}
-
-	var total int
-	for i := 0; i < len(arr); i++ {
-		total += arr[i]
-	}
-
-	return total
-
+	return max_so_far
 }
 
 func main() {
-	fmt.Println(maxSequent([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
+	fmt.Println(maxSequent([]int{-2, -3, 4, -1, -2, 1, 5, -3}))
 	fmt.Println(maxSequent([]int{1, 2, 3, 4, 5, 6}))
 	fmt.Println(maxSequent([]int{-7, 21, -11, 3, -1, 11}))
 }
