c00c6b87ca4c4567b5937f1ab71b93494530c26b
gozzafadillah <74459029+gozzafadillah@users.noreply.github.com>
2022-03-16 01:47:56 +0700

    push problem_1 analisis clean code

---
+++ 12_Clean_Code/praktikum/problem_1.go
@@ -0,0 +1,24 @@
+package main
+
+type user struct {
+	id       int
+	username int // username int ??
+	password int
+}
+
+type userservice struct { // menggunakan Camel case
+	t []user // tidak jelas t itu apa
+}
+
+func (u userservice) getallusera() []user { //penamaan fungsi lebih baik Camel case
+	return u.t
+}
+
+func (u userservice) getuserbyid(id int) user { // penamaan fungsi lebih baik Camel case
+	for _, r := range u.t { // variable r tidak jelas
+		if id == r.id {
+			return r
+		}
+	}
+	return user{}
+}
