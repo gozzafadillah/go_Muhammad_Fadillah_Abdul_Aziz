981fd8c00e8e8508140cd6efb07204d545879aa3
gozzafadillah <74459029+gozzafadillah@users.noreply.github.com>
2022-03-16 02:04:51 +0700

    Push problem 2 Rewrite to clean code

---
+++ 12_Clean_Code/praktikum/problem_2.go
@@ -0,0 +1,34 @@
+package main
+
+// Root struct
+type kendaraan struct {
+	totalRoda       int
+	kecepatanPerJam int
+}
+
+// Sub struct
+type Mobil struct {
+	kendaraan
+}
+
+// Mobil Start
+func (m *Mobil) Berjalan() {
+	m.tambahKecepatan(10)
+}
+
+// Menambahkan kecepatan
+func (m *Mobil) tambahKecepatan(kecepatanBaru int) {
+	output := m.kecepatanPerJam + kecepatanBaru
+	m.kecepatanPerJam = output
+
+}
+
+func main() {
+	mobilCepat := Mobil{}
+	mobilCepat.Berjalan()
+	mobilCepat.Berjalan()
+	mobilCepat.Berjalan()
+
+	mobilLamban := Mobil{}
+	mobilLamban.Berjalan()
+}
